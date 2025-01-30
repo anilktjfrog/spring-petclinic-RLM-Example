dd000a777267707bee1d6b55155b353a271f52d5
Krishna Manchikalapudi <krishnam@jfrog.com>
2025-01-30 12:17:43 -0800

    artifact evidence

--- my-files/readme.md
+++ my-files/readme.md
@@ -85,3 +85,5 @@ git push origin -f
 
 ## License
 The Spring PetClinic sample application is released under version 2.0 of the [Apache License](https://www.apache.org/licenses/LICENSE-2.0).
+
+<img src="./images/DayOne.png">
--- my-files/scripts-sh/jf-cli-evd.sh
+++ my-files/scripts-sh/jf-cli-evd.sh
@@ -5,7 +5,7 @@ clear
 # Config - Artifactory info
 export JF_HOST="psazuse.jfrog.io"  JFROG_RT_USER="krishnamanchikalapudi" JFROG_CLI_LOG_LEVEL="DEBUG" # JF_ACCESS_TOKEN="<GET_YOUR_OWN_KEY>"
 export JF_RT_URL="https://${JF_HOST}"
-export RT_REPO_VIRTUAL="krishnam-mvn-virtual" RBv2_SIGNING_KEY="krishnam"
+export RT_REPO_VIRTUAL="krishnam-mvn-virtual" RBv2_SIGNING_KEY="krishnam" EVD_SIGNING_KEY="KRISHNAM_JFROG_EVD_PUBLICKEY" 
 
 echo "JF_RT_URL: $JF_RT_URL \n JFROG_RT_USER: $JFROG_RT_USER \n JFROG_CLI_LOG_LEVEL: $JFROG_CLI_LOG_LEVEL \n "
 
@@ -44,22 +44,59 @@ echo "\n\n**** Build Info: Publish ****\n\n"
 jf rt bp ${BUILD_NAME} ${BUILD_ID} --detailed-summary
 
 
-# Evidence on build. Reference https://jfrog.com/help/r/jfrog-artifactory-documentation/evidence-management
-echo "\n\n**** Evidence: for Build ****\n\n"
+# Evidence. References
+#     Docs# https://jfrog.com/help/r/jfrog-artifactory-documentation/evidence-management
+#     CLI# https://docs.jfrog-applications.jfrog.io/jfrog-applications/jfrog-cli/binaries-management-with-jfrog-artifactory/evidence-service
+echo "\n\n**** Evidence: PACKAGE ****\n\n"
+ls -lrt ~/.ssh/jfrog_evd_*
 export KRISHNAM_JFROG_EVD_PRIVATEKEY="$(cat ~/.ssh/jfrog_evd_private.pem)" # ref: Create an RSA Key Pair https://jfrog.com/help/r/jfrog-artifactory-documentation/evidence-setup
 export KRISHNAM_JFROG_EVD_PUBLICKEY="$(cat ~/.ssh/jfrog_evd_public.pem)"
-ls -lrt ~/.ssh/jfrog_evd_*
-export VAR_EVD_JSON="evd-sign-${BUILD_ID}.json" 
-echo "{ \"actor\": \"krishnamanchikalapudi\", \"date\": \"$(date '+%Y-%m-%dT%H:%M:%SZ')\", \"build_name\": \"${BUILD_NAME}\", \"build_id\": \"${BUILD_ID}\", \"Job\":\"CMD SH Script\", \"Evd\":\"Build\" }" > ${VAR_EVD_JSON}
-cat ./${VAR_EVD_JSON}
 
-jf evd create --access-token="${JF_ACCESS_TOKEN}" --url="${JF_RT_URL}" --server-id="psazuse" --build-name ${BUILD_NAME} --build-number ${BUILD_ID} --predicate "./${VAR_EVD_JSON}" --predicate-type "https://dayone.dev/signature" --key ${KRISHNAM_JFROG_EVD_PRIVATEKEY}
-echo "🔎 Evidence attached on Build: signature 🔏 "
+
+# Evidence: Artifact
+export VAR_EVD_ARTIFACT_JSON="evd-artifact.json" 
+echo "{ \"actor\": \"krishnamanchikalapudi\", \"date\": \"$(date '+%Y-%m-%dT%H:%M:%SZ')\", \"build_name\": \"${BUILD_NAME}\", \"build_id\": \"${BUILD_ID}\", \"evd\":\"Evidence-Artifact\", \"artifact\":\"target/spring-petclinic-3.4.0-SNAPSHOT.jar\" }" > ${VAR_EVD_ARTIFACT_JSON}
+cat ./${VAR_EVD_ARTIFACT_JSON}
+
+
+spring-petclinic-3.4.0-SNAPSHOT.jar
+# jf evd create --predicate ./evd-artifact.json --predicate-type https://jfrog.com/evidence/build-signature/v1 --subject-repo-path krishnam-mvn-dev-local/org/springframework/samples/spring-petclinic/3.4.0-SNAPSHOT/spring-petclinic-3.4.0-20250130.180910-6.jar --key ~/.ssh/jfrog_evd_public.pem --key-alias "KRISHNAM_JFROG_EVD_PUBLICKEY"
+
+jf evd create --predicate ./evd-artifact.json --predicate-type https://jfrog.com/evidence/build-signature/v1 --subject-repo-path krishnam-mvn-dev-local/org/springframework/samples/spring-petclinic/3.4.0-SNAPSHOT/spring-petclinic-3.4.0-20250130.180910-6.jar --subject-sha256 "4015375be8d5e4a4311f11e341839c2d6b38b5c4a3fa19a61c8c1982a061a999" --key ~/.ssh/jfrog_evd_public.pem --key-alias "KRISHNAM_JFROG_EVD_PUBLICKEY"
+
+"target": "https://psazuse.jfrog.io/artifactory/",
+      "sha256": 
+
+jf evd create --predicate ./${VAR_EVD_ARTIFACT_JSON} --predicate-type https://jfrog.com/evidence/build-signature/v1 --subject-repo-path ${RT_REPO_VIRTUAL}/org/springframework/samples/spring-petclinic/3.4.0-SNAPSHOT/ --key ${KRISHNAM_JFROG_EVD_PRIVATEKEY} --key-alias ${EVD_SIGNING_KEY} 
+
+echo "🔎 Evidence attached on Artifact: signature 🔏 "
+
+
+
+# export VAR_EVD_PACKAGE_JSON="evd-package-${BUILD_ID}.json" 
+# echo "{ \"actor\": \"krishnamanchikalapudi\", \"date\": \"$(date '+%Y-%m-%dT%H:%M:%SZ')\", \"build_name\": \"${BUILD_NAME}\", \"build_id\": \"${BUILD_ID}\", \"job\":\"CMD SH Script\", \"evd\":\"Package Evidence\", \"artifact\":\"target/spring-petclinic-3.4.0-SNAPSHOT.jar\" }" > ${VAR_EVD_PACKAGE_JSON}
+# cat ./${VAR_EVD_PACKAGE_JSON}
+
+# # evd create --predicate /Users/jsmith/Downloads/code-review.json --predicate-type https://in-toto.io/attestation/vulns --package-name DockerPackage --package-version 1.0.0 --package-repo-key local-docker --key /Users/jsmith/Documents/keys/private.pem --key-alias xyzey
+# # jf evd create --predicate ./evd-package-cmd.evd.2025-01-29-16-32.json --predicate-type https://jfrog.com/evidence/build-signature/v1 --package-name spring-petclinic --package-version 1.0.0 --package-repo-name "krishnam-mvn-virtual" --key ~/.ssh/jfrog_evd_public.pem --key-alias "KRISHNAM_JFROG_EVD_PUBLICKEY" 
+# jf evd create --predicate ./${VAR_EVD_PACKAGE_JSON} --predicate-type https://jfrog.com/evidence/build-signature/v1 --package-name ${BUILD_NAME} --package-version ${BUILD_ID} --package-repo-name ${RT_REPO_VIRTUAL} --key ${KRISHNAM_JFROG_EVD_PRIVATEKEY} --key-alias ${EVD_SIGNING_KEY} 
+# echo "🔎 Evidence attached on Package: signature 🔏 "
+
+# jf evd create --predicate ./evd-package-cmd.evd.2025-01-29-16-32.json --predicate-type https://jfrog.com/evidence/build-signature/v1 --package-name spring-petclinic --package-version 3.4.0 --package-repo-name "krishnam-mvn-dev-local" --key ~/.ssh/jfrog_evd_public.pem --key-alias "KRISHNAM_JFROG_EVD_PUBLICKEY"
+
+# repo-path
+
+# # Evidence: Build Publish
+# # jf evd create --build-name spring-petclinic --build-number cmd.evd.2025-01-28-16-28 --predicate ./evd-artfact-cmd.evd.2025-01-28-16-28.json --predicate-type https://jfrog.com/evidence/build-signature/v1 --subject-repo-path target/spring-petclinic-3.4.0-SNAPSHOT.jar --key "$(cat ~/.ssh/jfrog_evd_private.pem)" key-name="KRISHNAM_JFROG_EVD_PUBLICKEY"
+# # jf evd create --build-name ${BUILD_NAME} --build-number ${BUILD_ID} --predicate ./$VAR_EVD_ARTIFACT_JSON --predicate-type https://jfrog.com/evidence/build-signature/v1 --subject-repo-path target/spring-petclinic-3.4.0-SNAPSHOT.jar --key ${KRISHNAM_JFROG_EVD_PRIVATEKEY} --key-name "KRISHNAM_JFROG_EVD_PUBLICKEY" 
+# #echo "🔎 Evidence attached on Build: signature 🔏 "
 
 
 sleep 3
 echo "\n\n**** CLEAN UP ****\n\n"
-rm -rf ${VAR_EVD_JSON}
+ls evd-*.json
+# rm -rf ${VAR_EVD_ARTIFACT_JSON}
+# rm -rf ${VAR_EVD_PACKAGE_JSON}
 
 set +x # stop debugging from here
 echo "\n\n**** JF-CLI-EVD-RBV2.SH - DONE at $(date '+%Y-%m-%d-%H-%M') ****\n\n"
